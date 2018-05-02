7ee3ed7fd3e829688114b2b5f53fd697a9d742d8
Andy M <admazzola@gmail.com>
2018-05-02 09:54:46 -0400

    config files

---
+++ sample.account.config.js
@@ -0,0 +1,10 @@
+var account = {
+
+  relay: {
+    address: '0xf3243bAbF74Ead828ac656877137Df705868fD66',
+    privateKey: '00000'
+  }
+ 
+}
+
+exports.account = account
---
+++ sample.relay.config.js
@@ -0,0 +1,15 @@
+
+var relayconfig = {
+  seedNodes: [
+    10.10.10.10:9774,
+    
+  ],
+  priceOracleURL: null,
+  minGasPriceWei: 10,
+  maxGasPriceWei: 6,
+  minProfitFactor: 1.5,
+  web3provider: 'http://localhost:8545'
+}
+
+
+exports.config = relayconfig;
