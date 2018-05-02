db96d02edb59d0bf654fb8a6f183a23fea60d4aa
Andy M <admazzola@gmail.com>
2018-05-02 16:39:10 -0400

    seed node

--- sample.relay.config.js
+++ sample.relay.config.js
@@ -2,15 +2,15 @@
 var relayconfig = {
   name:'A cool relay node',
   seedNodes: [
-    {name:'main1',address:'10.10.10.10:9774'},
+    {address:'104.131.112.200:9774'},
 
   ],
   priceOracleURL: null,
   minGasPriceWei: 10,
   maxGasPriceWei: 6,
   minProfitFactor: 1.5,
-  web3provider: 'http://localhost:8545',
 
+  web3provider: 'http://localhost:8545',
   allowTestPackets: true,
 }
 
