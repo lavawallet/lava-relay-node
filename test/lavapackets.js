dcb7f322730b4bbac174f956fc22179e39e3e1b2
andy <andy@jonezie.com>
2019-03-14 01:29:35 -0400

    fixing tests again

--- test/lavapackets.js
+++ test/lavapackets.js
@@ -3,6 +3,8 @@ var INFURA_ROPSTEN_URL = 'https://ropsten.infura.io/gmXEVo5luMPUGPqg6mhy';
 
 var LavaPeerInterface = require('../lib/lava-peer-interface');
 
+var LavaPacketSubmitter = require('../lib/lava-packet-submitter');
+
 var redisInterface = require('../lib/redis-interface')
 var ContractInterface = require('../lib/contract-interface')
 
@@ -146,6 +148,31 @@ var lavaPeerInterface;
 
 
       //submit the relay packet - use ganache
+
+
+      it('submit the packet', async function() {
+
+            var packetData = {
+              methodName: 'transfer',
+              relayAuthority: '0x0000000000000000000000000000000000000000',
+              from: "0xb11ca87e32075817c82cc471994943a4290f4a14",
+              to: "0x357FfaDBdBEe756aA686Ef6843DA359E2a85229c",
+              wallet:"0x1d0d66272025d7c59c40257813fc0d7ddf2c4826",
+              tokens:200000000,
+              relayerRewardTokens:100000000,
+              expires:3365044,
+              nonce:"0xc28f687c56f1b2749af7d6151fa351", //wrong nonce for sig
+              signature:"0x8ef27391a81f77244bf95df58737eecac386ab9a47acd21bdb63757adf71ddf878169c18e4ab7b71d60f333c870258a0644ac7ade789d59c53b0ab75dbcc87d11b"
+          }
+
+          var response =  await LavaPacketSubmitter.broadcastLavaPacket(packetData,'normal',web3,'development');
+          console.log('broadcast',response)
+          assert.equal( response  , true );
+
+
+      });
+
+
 
       /*
 
